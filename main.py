cc67ccd8b0324a3d28478332e5b1b275fe98d0ce
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-12-25 15:19:57 -0500

    Refactor parameter type checking in get_param_type function

--- main.py
+++ main.py
@@ -10,6 +10,35 @@ def get_functions_from_file(filename: str) -> List:
     filename_without_ext = os.path.splitext(filename)[0]
     return [func for func in inspect.getmembers(__import__(filename_without_ext)) if inspect.isfunction(func[1])]
 
+# https://chat.openai.com/share/ba42c1ef-c614-4caf-9d25-4a15afefedcc
+def get_param_type(param):
+    # Check if param has a type annotation
+    if param.annotation != inspect._empty:
+        param_type = str(param.annotation)
+        
+        # Check for common types and convert them
+        if 'int' in param_type:
+            return 'number'
+        elif 'float' in param_type:
+            return 'number'
+        elif 'str' in param_type:
+            return 'string'
+        elif 'bool' in param_type:
+            return 'boolean'
+        elif 'list' in param_type:
+            return 'array'
+        elif 'dict' in param_type:
+            return 'object'
+        elif 'datetime.date' in param_type or 'datetime.datetime' in param_type:
+            return 'date-time'
+        # Add more type checks here if needed
+        
+        # Fallback for any other types
+        return param_type
+    else:
+        # Default to string if no type annotation is provided
+        return "string"
+
 def main():
     files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.py')]
     questions = [
@@ -39,7 +68,7 @@ def main():
         for param in inspect.signature(func[1]).parameters.values():
             param_name = param.name
             # if annotation is _empty, then it is not annotated, and use str as default
-            param_type = param.annotation if param.annotation != inspect._empty else "string"
+            param_type = get_param_type(param)
             param_desc = typer.prompt(f"Description for parameter '{param_name}'", type=str)
             is_required = typer.confirm(f"Is the param '{param_name}' required?", default=False)
             
---
+++ test-typed.py
@@ -0,0 +1,53 @@
+from typing import List
+import datetime
+
+# fibonacci function
+def fib(n: int) -> None:
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a+b
+    print()
+
+# get tomorrow's date
+def tomorrow() -> str:
+    return (datetime.date.today() + datetime.timedelta(days=1)).isoformat()
+
+# mergesort
+def mergesort(arr: List[int]) -> None:
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        
+        mergesort(left)
+        mergesort(right)
+        
+        i = j = k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i+=1
+            else:
+                arr[k] = right[j]
+                j+=1
+            k+=1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j+=1
+            k+=1
+
+# username and email validation
+def validate(username: str, email: str) -> bool:
+    if len(username) < 3:
+        return False
+    if len(email) < 3:
+        return False
+    return True
